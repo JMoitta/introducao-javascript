f421f98bc259ed76258306df263d576c590182ac
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-11-12 17:59:17 -0300

    Aula 02:07 - Operadores Lógicos

--- js/principal.js
+++ js/principal.js
@@ -9,7 +9,24 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura); 
-
 var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
+
+var pesoEhValido = true;
+var alturaEhValido = true;
+
+if(peso < 0 || peso > 1000) {
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido";
+}
+
+if(altura < 0 || altura > 3.00){
+    console.log("Altura inválida")
+    alturaEhValido = false;
+    tdImc.textContent = "Altura Inválido";
+}
+
+if(pesoEhValido && alturaEhValido){
+    var imc = peso / (altura * altura); 
+    tdImc.textContent = imc;
+}
